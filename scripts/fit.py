4bfd8de36017ec36b927be1609dfc872aaa5935d
Alexdemenev <demenev2001@mail.ru>
2025-04-23 20:04:45 +0000

    Change model to LogisticRegression

--- scripts/fit.py
+++ scripts/fit.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -31,14 +32,14 @@ def fit_model():
     preprocessor = ColumnTransformer(
         [
             ('binary', OneHotEncoder(drop=params['one_hot_drop']), binary_cat_features.columns.tolist()),
-            ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
+            ('cat', OneHotEncoder(drop=params['one_hot_drop']), other_cat_features.columns.tolist()),
             ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
 
-    model = CatBoostClassifier(auto_class_weights=params['auto_class_weights'])
+    model = LogisticRegression(C=params['C_regr'], penalty=params['penalty'])
 
     pipeline = Pipeline(
         [
